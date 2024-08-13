5898cdacf05b5d481b6a6feaca83332c40c629b2
Mike Lebake <131595937+mikelebake@users.noreply.github.com>
2024-08-13 22:18:02 +0200

    Add files via upload

--- myproject/urls.py
+++ myproject/urls.py
@@ -25,4 +25,14 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),  # URL de connexion
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # URL de déconnexion
     path('', accounts_views.home, name='home'),  # URL de la page d'accueil
+    
+    # Password change views
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    # Password reset views
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
